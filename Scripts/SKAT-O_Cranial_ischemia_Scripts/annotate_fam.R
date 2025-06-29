6aa519ba28c1f492f48caeacb7fb3b42314a0b91
ChristopherLetton <154468191+ChristopherLetton@users.noreply.github.com>
2025-06-29 21:10:32 +0100

    Add files via upload

---
+++ Scripts/SKAT-O_Cranial_ischemia_Scripts/annotate_fam.R
@@ -0,0 +1,29 @@
+setwd("/scratch/jsxm6270/gca_skat_V2/data")
+
+library(dplyr)
+library(readr)
+
+# Load files
+pheno <- read_csv("Regeneron UK GCA clinical data Nov 2024 - Copy.csv")
+linker <- read_csv("Regeneron UK GCA ID Key.csv")
+
+# Join phenotype and linker by 'Adm_RegDatabaseID'
+pheno_joined <- linker %>%
+  inner_join(pheno, by = "Adm_RegDatabaseID")
+
+# Annotate cases and controls
+pheno_joined <- pheno_joined %>%
+  mutate(PHENO = ifelse(CIMtotal == "any cranial ischaemic complications (i.e. permanent)", 2, 1))
+
+# Load original .fam file
+fam <- read.table("/scratch/jsxm6270/gca_skat_V2/LEEDS_Freeze_One.GL.splitmulti.fam", header=FALSE)
+
+# Update .fam phenotype column
+fam_updated <- fam %>%
+  left_join(pheno_joined, by = c("V2" = "Sample Name")) %>%
+  mutate(V6 = ifelse(is.na(PHENO), 0, PHENO)) %>%
+  select(V1:V6)
+
+# Save updated .fam
+write.table(fam_updated, file="/scratch/jsxm6270/gca_skat_V2/data/LEEDS_CIpheno.fam",
+            col.names=FALSE, row.names=FALSE, sep="\t", quote=FALSE)
