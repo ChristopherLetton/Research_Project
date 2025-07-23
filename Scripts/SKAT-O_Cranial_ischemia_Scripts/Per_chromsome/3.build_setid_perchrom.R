4d40dea2b8e875e63b448b119a0f690284ca70d5
ChristopherLetton <154468191+ChristopherLetton@users.noreply.github.com>
2025-07-23 15:05:32 +0100

    Update 3.build_setid_perchrom.R

--- Scripts/SKAT-O_Cranial_ischemia_Scripts/Per_chromsome/3.build_setid_perchrom.R
+++ Scripts/SKAT-O_Cranial_ischemia_Scripts/Per_chromsome/3.build_setid_perchrom.R
@@ -1,19 +1,40 @@
 args <- commandArgs(trailingOnly = TRUE)
 chr  <- args[1]
+
 base <- "/scratch/gca_skat_V2"
 work <- file.path(base, "chr_work", paste0("chr", chr))
 
 .libPaths(c("/scratch/R_libs", .libPaths()))
 library(readr); library(dplyr)
 
+coding_file <- "/scratch/ref/protein_coding_ensembl_ids.txt"
+pc_genes    <- read_lines(coding_file)
+
+# rare-variant IDs for this chromosome
 rare <- read_lines(file.path(work, paste0("rare_chr", chr, ".snplist")))
 
-vep  <- read_tsv(file.path(base, "data", "LEEDS_CIpheno.vep.tsv"),
-                 comment="##", show_col_types=FALSE) |>
-        select(SNP_ID = `#Uploaded_variation`, GeneID = Gene) |>
-        filter(SNP_ID %in% rare, !is.na(GeneID), GeneID != "") |>
-        distinct(GeneID, SNP_ID)
+vep <- read_tsv
+          file.path(base, "data", "LEEDS_CIpheno.vep.tsv"),
+          comment = "##", show_col_types = FALSE
+       ) |>
+       select(
+         SNP_ID = `#Uploaded_variation`,
+         GeneID = Gene
+       ) |>
+       filter(
+         SNP_ID %in% rare,
+         GeneID %in% pc_genes,     
+         !is.na(GeneID),
+         GeneID != ""
+       ) |>
+       distinct(GeneID, SNP_ID)
 
-out  <- file.path(work, paste0("SetID_chr", chr, ".txt"))
-write.table(vep, file = out, sep = "\t",
-            quote = FALSE, row.names = FALSE, col.names = FALSE)
+out <- file.path(work, paste0("SetID_chr", chr, ".txt"))
+write.table(
+  vep,
+  file = out,
+  sep  = "\t",
+  quote = FALSE,
+  row.names = FALSE,
+  col.names = FALSE
+)
